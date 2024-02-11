24a5cde8aa93aa7cdb07d50fa41a3b76bbecda68
anis06 <34106573+anis06@users.noreply.github.com>
2024-02-12 00:26:20 +0100

    Update footer.blade.php

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -38,7 +38,6 @@
                 </ul>
             </div>
             <div>
-                <h3 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Company</h3>
                 <ul class="text-gray-500 dark:text-gray-400">
                     <li class="mb-4">
                         <a href="#" class=" hover:underline">About</a>
