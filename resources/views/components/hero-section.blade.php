b94d99ce277102c31cecf7c2df127f97835243cf
anis06 <34106573+anis06@users.noreply.github.com>
2024-02-12 01:03:49 +0100

    Update hero-section.blade.php

--- resources/views/components/hero-section.blade.php
+++ resources/views/components/hero-section.blade.php
@@ -4,14 +4,13 @@
             <h1
                 class="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">
                 Prenez votre <br> <span class="text-green-400">IPTV</span> en toute <br> simplicite</h1>
-            <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">This free
-                and open-source landing page template was built using the utility classes from <a
-                    href="https://tailwindcss.com" class="hover:underline">Tailwind CSS</a> and based on the components
+            <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Découvrez la meilleure expérience IPTV avec notre sélection exceptionnelle comprenant Orca Pro Plus, Iron TV Pro, King365 IPTV, Atlas Pro IPTV, et Gogo IPTV. Profitez de matchs en direct, films en streaming, et une incroyable variété de 70000 chaînes et 80000 VOD films et séries en qualité 4K et FHD. Optez pour une solution IPTV de qualité à un prix abordable et Transformez votre divertissement avec notre offre exceptionnelle. <a
+                    href="#" class="hover:underline">#</a> and based on the components
                 from the</p>
             <div class="space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
                 <a href="#"
                     class="inline-flex dark:bg-green-400 items-center justify-center w-auto px-5 py-3 mb-2 mr-2 text-sm dark:font-bold text-gray-900border border-gray-200 rounded-lg sm:w-auto focus:outline-none hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:text-gray-900 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-                    Get started
+                    Profitez 
                 </a>
             </div>
         </div>
